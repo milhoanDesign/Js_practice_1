44a222ad3337a055e5f1f21cd19e838e70dbaf2b
Walker Milhoan <walker@milhoandesign.com>
2024-05-08 17:13:33 -0600

    Added a select field for guests

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
     // - Get references to the form inputs, the output list, and the buttons by their IDs
      const firstName = document.getElementById('firstName');
      const lastName = document.getElementById('lastName');
+     const guestCountSelect = document.getElementById('selectGuests');
      const submitButton = document.getElementById('submitButton');
      const clearButton = document.getElementById('clearButton'); 
      const outputList = document.getElementById('outputList'); 
@@ -18,11 +19,12 @@ document.addEventListener('DOMContentLoaded', function() {
         // - Get the values entered into the "First Name" and "Last Name" fields
         const firstNameValue = firstName.value.trim(); 
         const lastNameValue = lastName.value.trim(); 
+        const guestCount = guestCountSelect.value; 
         
         // - If both fields are filled, create a new list item combining the names
         if (firstNameValue && lastNameValue) {
             const listItem = document.createElement("li"); 
-            listItem.textContent = `${firstNameValue} ${lastNameValue}`; 
+            listItem.textContent = `${firstNameValue} ${lastNameValue}. Number of guests: ${guestCount}`; 
             
             // - Append the new list item to the "outputList" (Guest List) section
             outputList.appendChild(listItem); 
@@ -30,6 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // - Clear the input fields after submission
             firstName.value = "";
             lastName.value = "";
+            guestCountSelect.value = '0'; 
 
             //Set the focus back to the firstName
             firstName.focus(); 
@@ -45,6 +48,11 @@ document.addEventListener('DOMContentLoaded', function() {
         // - Set the "First Name" and "Last Name" input fields to empty strings
         firstName.value = "";
         lastName.value = ""; 
+        
+        // - Set the select field back to 0
+        guestCountSelect.value = '0';
+
+        // - Clear the list items
         outputList.innerHTML = ""; 
         firstName.focus(); 
          
